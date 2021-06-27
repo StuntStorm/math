7e9a87c555ad2d459a8bd92f07fecfb26d884533
Rizwan Nizarudin <56226566+StuntStorm@users.noreply.github.com>
2021-06-27 09:57:56 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,50 @@
+module.exports = {
+    add : function (a, b) {
+        return a + b;
+    },
+    add3: function (a, b,c) {
+        return a + b+c;
+    },
+    add4: function (a, b,c,d) {
+        return a + b+c+d;
+    },
+    add5: function (a, b,c,d,e) {
+        return a + b+c+d+e;
+    },
+    subtract : function (a, b) {
+        return a - b;
+    },
+    subtract3: function (a, b, c) {
+        return a - b-c;
+    },
+    subtract4: function (a, b, c, d) {
+        return a - b-c-d;
+    },
+    subtract5: function (a, b, c, d, e) {
+        return a - b-c-d-e;
+    },
+    multiply : function (a, b) {
+        return a * b;
+    },
+    divide : function (a, b) {
+        return a / b;
+    },
+    multiply3: function (a, b,c) {
+        return a * b*c;
+    },
+    divide3: function (a, b,c) {
+        return a / b/c;
+    },
+    multiply4: function (a, b,c,d) {
+        return a * b*c*d;
+    },
+    divide4: function (a, b,c,d) {
+        return a / b/c/d;
+    },
+    multiply5: function (a, b,c,d,e) {
+        return a * b*c*d*e;
+    },
+    divide5: function (a, b,c,d,e) {
+        return a / b/c/d/e;
+    },
+}
